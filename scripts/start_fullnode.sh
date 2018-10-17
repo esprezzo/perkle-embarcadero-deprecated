1eda637a645685472cc19bad14e38253094e3c8c
errata <mutexdev@protonmail.com>
2018-10-16 22:59:11 -0400

    start script update

--- scripts/start_fullnode.sh
+++ scripts/start_fullnode.sh
@@ -7,7 +7,6 @@
     --rpcaddr '0.0.0.0' \
     --rpcport 8501 \
     --rpcapi 'db,eth,net,web3,txpool,miner,personal' \
-    # --gasprice '1' \
     --etherbase '0x8C12E659ACE965C4A17F3664135E6BB0292789E0' \
     --nodekey ~/boot.key \
     --verbosity 3 \
