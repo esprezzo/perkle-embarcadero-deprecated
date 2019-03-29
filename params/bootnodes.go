d409fb22e7dd29c469583c2449c9590144f2f9f7
a <email@example.com>
2019-03-29 11:32:15 -0700

    seed updates

--- params/bootnodes.go
+++ params/bootnodes.go
@@ -21,9 +21,9 @@ package params
 
 var MainnetBootnodes = []string{
 	// Esprezzo Foundation Go Bootnodes
-	"enode://enode://9a07e80df806bfacd89b4245f8272ced37ff4985e7db98d7c1a4883cf8c49c89e970d3b28bd2f986e97cace01af9a78e117478d4927b2d65c306a4622a2ff4b0@3.90.100.66:30310", // BOOTNODE/Blocknet2
-	"enode://9a07e80df806bfacd89b4245f8272ced37ff4985e7db98d7c1a4883cf8c49c89e970d3b28bd2f986e97cace01af9a78e117478d4927b2d65c306a4622a2ff4b0@3.90.100.66:30311",         // Blocknet 1
-	"enode://0757ba56ff97496c32dfc586193f917411c0364b5a4892db79e52e1a36b410430d87767d40afbe6e5c94c4a72d1b59db90fa4262bd52f0ea8d0a4e117cc11390@34.232.227.76:30311",       // Blocknet 3
+	"enode://9a07e80df806bfacd89b4245f8272ced37ff4985e7db98d7c1a4883cf8c49c89e970d3b28bd2f986e97cace01af9a78e117478d4927b2d65c306a4622a2ff4b0@3.90.100.66:30310",   // BOOTNODE/Blocknet2
+	"enode://d8f8a7cfe05231426431f489430e880d6fb7f425d49dc1a95abef589c3c6b541d280dcdf1c6ad12b0d343771b909dd021a85d7d47e751269854d57f5cf1be4d1@35.168.54.62:30311",  // Blocknet 1
+	"enode://0757ba56ff97496c32dfc586193f917411c0364b5a4892db79e52e1a36b410430d87767d40afbe6e5c94c4a72d1b59db90fa4262bd52f0ea8d0a4e117cc11390@34.232.227.76:30311", // Blocknet 3
 }
 
 // TestnetBootnodes are the enode URLs of the P2P bootstrap nodes running on the
@@ -37,4 +37,3 @@ var RinkebyBootnodes = []string{}
 // DiscoveryV5Bootnodes are the enode URLs of the P2P bootstrap nodes for the
 // experimental RLPx v5 topic-discovery network.
 var DiscoveryV5Bootnodes = []string{}
-
